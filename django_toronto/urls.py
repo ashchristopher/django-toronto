524c31acb2da1358c7e70f3c8c672fd431dc0ff9
Ash Christopher <ash.christopher@gmail.com>
2012-07-01 14:57:23 -0700

    Renamed site-media to static-media

--- django_toronto/urls.py
+++ django_toronto/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 admin.autodiscover()
 
@@ -9,3 +10,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls')),
 )
+
+urlpatterns += staticfiles_urlpatterns()
