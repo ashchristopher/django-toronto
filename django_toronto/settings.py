82f3a7c25feb4bdd22fb3b6bef020e05c1887278
Ash Christopher <ash.christopher@gmail.com>
2012-07-01 16:38:32 -0700

    Added homepage

--- django_toronto/settings.py
+++ django_toronto/settings.py
@@ -110,6 +110,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
+    'django.contrib.webdesign',
     'gunicorn',
     'registration',
     'bootstrap',
--- django_toronto/urls.py
+++ django_toronto/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # url(r'^$', 'django_toronto.views.home', name='home'),
+    url(r'^$', TemplateView.as_view(template_name='homepage.html'), name='home'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls')),
