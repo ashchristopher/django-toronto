7074e1c3c1daba164ca8d7e24c73e23212991e89
Ash Christopher <ash.christopher@gmail.com>
2012-06-30 18:53:13 -0700

    Added registration

--- django_toronto/settings.py
+++ django_toronto/settings.py
@@ -1,5 +1,8 @@
+import os.path
 import dj_database_url
 
+
+PROJECT_PATH = os.path.join(os.path.dirname(__file__), '../')
 
 DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
@@ -101,6 +104,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'django_toronto', 'templates')
 )
 
 INSTALLED_APPS = (
