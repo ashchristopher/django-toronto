0c7f9506417e17dacb605f18a5e757ff1ac6d085
Ash Christopher <ash.christopher@gmail.com>
2012-07-08 12:12:09 -0700

    Get the latest event properly

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+
 from django.views.generic import TemplateView
+from django.utils.timezone import utc
 
 from events.models import Event
 from presentations.models import Presentation
@@ -8,9 +11,12 @@ class HomepageView(TemplateView):
     template_name = 'homepage.html'
 
     def get(self, request):
+        now = datetime.now().replace(tzinfo=utc)
+
+        next_event = Event.objects.filter(date__gte=now).order_by('date')[:1]
 
         context = {
-            'next_event': Event.objects.all()[0],
+            'next_event': next_event,
             'latest_presentations': Presentation.objects.all().order_by('-pk')[0:4],
         }
 
