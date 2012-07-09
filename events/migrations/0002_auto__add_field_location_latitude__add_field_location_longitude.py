8b678e4a49a4080eaa2feac072aec42a3da41b7b
Ash Christopher <ash.christopher@gmail.com>
2012-07-08 19:12:49 -0700

    Schema changes.
    * added index to date (since we do lookups using this field)
    * added presentations to the events
    * added lat/long to event locations

---
+++ events/migrations/0002_auto__add_field_location_latitude__add_field_location_longitude.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding M2M table for field presentations on 'Event'
+        db.create_table('events_event_presentations', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('event', models.ForeignKey(orm['events.event'], null=False)),
+            ('presentation', models.ForeignKey(orm['presentations.presentation'], null=False))
+        ))
+        db.create_unique('events_event_presentations', ['event_id', 'presentation_id'])
+
+        # Adding index on 'Event', fields ['date']
+        db.create_index('events_event', ['date'])
+
+        # Adding field 'Location.latitude'
+        db.add_column('events_location', 'latitude',
+                      self.gf('django.db.models.fields.FloatField')(default=0.0),
+                      keep_default=False)
+
+        # Adding field 'Location.longitude'
+        db.add_column('events_location', 'longitude',
+                      self.gf('django.db.models.fields.FloatField')(default=0.0),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Removing index on 'Event', fields ['date']
+        db.delete_index('events_event', ['date'])
+
+        # Removing M2M table for field presentations on 'Event'
+        db.delete_table('events_event_presentations')
+
+        # Deleting field 'Location.latitude'
+        db.delete_column('events_location', 'latitude')
+
+        # Deleting field 'Location.longitude'
+        db.delete_column('events_location', 'longitude')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'events.event': {
+            'Meta': {'object_name': 'Event'},
+            'date': ('django.db.models.fields.DateTimeField', [], {'db_index': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'location': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['events.Location']"}),
+            'presentations': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['presentations.Presentation']", 'symmetrical': 'False'})
+        },
+        'events.location': {
+            'Meta': {'object_name': 'Location'},
+            'address': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'latitude': ('django.db.models.fields.FloatField', [], {}),
+            'longitude': ('django.db.models.fields.FloatField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '120'})
+        },
+        'presentations.presentation': {
+            'Meta': {'object_name': 'Presentation'},
+            'date_added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'embed_code': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'oembed': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '120'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+        }
+    }
+
+    complete_apps = ['events']
--- events/models.py
+++ events/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Location(models.Model):
     name = models.CharField(max_length=120)
     address = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
 
     def __unicode__(self):
         return self.name
@@ -11,7 +13,8 @@ class Location(models.Model):
 
 class Event(models.Model):
     location = models.ForeignKey('Location')
-    date = models.DateTimeField()
+    presentations = models.ManyToManyField('presentations.Presentation')
+    date = models.DateTimeField(db_index=True)
 
     def __unicode__(self):
         return "{location} - {date}".format(location=self.location.name, date=self.date)
