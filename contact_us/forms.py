1170c1d3a0a1dd248527cb1c737503eb37f6578d
Ash Christopher <ash.christopher@gmail.com>
2012-07-16 21:17:46 -0700

    Updated form validation to not accept from ips that are banned

--- contact_us/forms.py
+++ contact_us/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from contact_us.models import ContactUsMessage
+from contact_us.models import ContactUsMessage, BannedIp
 
 
 class ContactUsMessageForm(forms.ModelForm):
@@ -9,3 +9,12 @@ class ContactUsMessageForm(forms.ModelForm):
     class Meta:
         model = ContactUsMessage
         fields = ('email_address', 'message', )  # we will only accept the message as user input
+
+    def clean(self):
+        cleaned_data = super(ContactUsMessageForm, self).clean()
+        ip_data = self.instance.submitter_ip
+
+        if BannedIp.objects.filter(banned_ip=ip_data).exists():
+            raise forms.ValidationError("This ip ({ip}) has been banned due to abuse.".format(ip=ip_data))
+
+        return cleaned_data
